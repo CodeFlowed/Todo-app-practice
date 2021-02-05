153f7108fa2164e89b4316096d975b5ddddab6b4
cybermon <cybermon@CyberMon.local>
2021-02-05 12:47:39 -1000

    Redo js button listeners for Done and Delete buttons to use js delegation

--- app.js
+++ app.js
@@ -4,34 +4,27 @@ function init() {
     addItemButton.onclick = handleAddItemButton; // ### see if .onsubmit works better here
     var addItemInput = document.getElementById("userInput");
     addItemInput.onkeypress = handleKeypress;
-
-    // listen for individual task button inputs for "Done" buttons
-    addDoneBtnListeners();
-
-    // listen for individual task button inputs for "Delete" buttons
-    document.querySelectorAll(".task__delete-btn").forEach(
-        function (item) {
-            item.addEventListener("click", handleDelete, false);
-        }
-    );
 }
 
-function addDoneBtnListeners() {
-    document.querySelectorAll(".task__done-btn").forEach(
-        function (item) {
-            item.addEventListener("click", handleMarkedDone, false);
-        }
-    );
-}
+document.addEventListener("click", function (event) {
+    // If Done button is clicked, add .task--done to the parent li 
+    if (event.target.matches(".task__done-btn")) {
+        handleDoneBtn(event);
+    }
+
+    // If Delete button is clicked, remove parent li
+    if (event.target.matches(".item__delete-btn")) {
+        handleDeleteBtn(event);
+    }
+}, false);
 
 function handleAddItemButton() {
     var newItemToProcess = document.getElementById("userInput").value;
     // get the ul#bucket and add newItemToProcess to a span with accompanying buttons and place it inside a new li element
     document.getElementById("bucket").innerHTML +=
         `<li class="item task">
-            <span>${newItemToProcess}</span><button class="item__btn task__done-btn">✔️</button><button class="item__btn task__delete-btn">🗑️</button>
+            <span>${newItemToProcess}</span><button class="item__btn task__done-btn">✔️</button><button class="item__btn item__delete-btn">🗑️</button>
         </li>`;
-    addDoneBtnListeners(); // ### why not make this add only 1 listener so it will be more efficient?
 }
 
 function handleKeypress(e) {
@@ -41,15 +34,15 @@ function handleKeypress(e) {
     }
 }
 
-function handleMarkedDone() {
-    // get the appropriate li and change its class to .task--done
-    var liParent = this.parentElement;
+function handleDoneBtn(event) {
+    var liParent = event.target.parentElement;
     liParent.classList.toggle("task--done");
 }
 
-function handleDelete() {
-    // get the appropriate li and delete it from the DOM
-    var liToDelete = this.parentElement;
-    document.getElementById("bucket").removeChild(liToDelete);
+function handleDeleteBtn(event) {
+    // get the appropriate li and remove it
+    var itemToRemove = event.target.parentElement;
+    document.getElementById("bucket").removeChild(itemToRemove);
 }
+
 window.onload = init;
